fc3f9c13bb1ae0a263db991ed263c1ad85422191
Yigit Kerem Oktay <yigitkerem@theskyfallen.com>
2021-04-15 16:23:17 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, shell, ipcMain } = require('electron')
 const path = require('path')
-const expressServer = require("./express")
+var express = require("express")
+const request = require('request');
+var wapp = express();
+var wserver
 var win
+var authToken
+
+var callbackUri = "http://localhost:12971/callback"
+var idmsaUri = "https://idmsa.idledev.org"
+var idmsaId = "BMHpIDSRBGSxRhfA1cCAfKQEnksQRvl71Gn3x7g4"
+var idmsaSecret = "0M4frXRRaiMyVEu0Wcmwp8hnyEzt0xF1I67ZPnsb"
 
 function createWindow () {
     if(process.platform === 'darwin'){
@@ -29,8 +38,7 @@ function createWindow () {
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  expressServer.init();
+    createWindow()
 
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) {
@@ -39,8 +47,54 @@ app.whenReady().then(() => {
   })
 })
 
+ipcMain.on('init',function(e,a){
+
+    if(authToken === undefined){
+        initWASession()
+        var _flagCheck = setInterval(function() {
+            if (authToken !== undefined) {
+                clearInterval(_flagCheck);
+                win.webContents.send('auth',authToken)
+            }
+        }, 100);
+    } else {
+        win.webContents.send('auth',authToken)
+    }
+
+})
+
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
   }
 })
+
+async function initWebAuth() {
+
+    wapp.get("/callback", (req, res) => {
+        res.send("Authorisation complete, you may safely return back to the app.")
+        request.post({
+            url: idmsaUri+"/oauth/token",
+            form: {
+              grant_type: 'authorization_code',
+              code: req.query.code,
+              client_id: idmsaId,
+              client_secret: idmsaSecret,
+              redirect_uri: callbackUri
+            }
+        }, function (err, httpResponse, body) { 
+            authToken = JSON.parse(body).access_token
+         })
+        wserver.close()
+    })
+
+    wserver = wapp.listen(12971)
+}
+
+async function initWASession(){
+
+    await initWebAuth();
+
+    shell.openExternal(`${idmsaUri}/oauth/authorize?client_id=${idmsaId}&redirect_uri=${encodeURIComponent(callbackUri)}&response_type=code`)
+
+}
